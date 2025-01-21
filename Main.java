b98e34685e14bf3a35e100c70b15f072f6d664f7
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-01-21 13:54:34 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        int [] arr= {5,7,6,9,4,3};
+        int item =4;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==item){
+        
+        System.out.println("array present is : "+ i + " index position is ");
+    
+}
+}
+}
+}
